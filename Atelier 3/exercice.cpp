67260cfe5cbcc09034644d2e847d5a11237160ad
Akmam <aladine.akmam@etu.uae.ac.ma>
2024-10-22 20:30:32 +0100

    Update exercice.cpp

--- Atelier 3/exercice.cpp
+++ Atelier 3/exercice.cpp
@@ -19,7 +19,6 @@ public:
         vitesse = 0.0;
     }
 
-    // Constructeur avec paramètres
     Voiture(string m, string mod, int a, float km, float v) {
         marque = m;
         modele = mod;
@@ -27,13 +26,10 @@ public:
         kilometrage = km;
         vitesse = v;
     }
-
-    // Méthode pour accélérer
     void accelerer(float valeur) {
         vitesse += valeur;
     }
 
-    // Méthode pour freiner
     void freiner(float valeur) {
         if (vitesse - valeur >= 0) {
             vitesse -= valeur;
@@ -41,8 +37,6 @@ public:
             vitesse = 0;
         }
     }
-
-    // Méthode pour afficher les informations
     void afficherInfo() const {
         cout << "Marque: " << marque << endl;
         cout << "Modèle: " << modele << endl;
@@ -50,35 +44,28 @@ public:
         cout << "Kilométrage: " << kilometrage << " km" << endl;
         cout << "Vitesse: " << vitesse << " km/h" << endl;
     }
-
-    // Méthode pour avancer
     void avancer(float distance) {
         kilometrage += distance;
     }
 
-    // Destructeur
     ~Voiture() {
         cout << "La voiture " << marque << " " << modele << " est détruite." << endl;
     }
 };
 
 int main() {
-    // Création d'une voiture avec le constructeur par défaut
     Voiture voiture1;
     voiture1.afficherInfo();
 
-    // Création d'une voiture avec des paramètres
     Voiture voiture2("Toyota", "Corolla", 2020, 10000, 120);
     voiture2.afficherInfo();
 
-    // Accélérer et freiner la voiture
     voiture2.accelerer(20);
     voiture2.afficherInfo();
 
     voiture2.freiner(50);
     voiture2.afficherInfo();
 
-    // Avancer la voiture
     voiture2.avancer(500);
     voiture2.afficherInfo();
 
